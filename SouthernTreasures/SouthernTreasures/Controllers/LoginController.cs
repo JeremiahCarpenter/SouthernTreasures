6d5d4d12e0590098d5d01dadd87194bd9945e5ce
SeanLeeDev <33349300+SeanLeeDev@users.noreply.github.com>
2019-11-06 16:44:22 -0600

    Add files via upload

---
+++ SouthernTreasures/SouthernTreasures/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using SouthernTreasures.Users.Model;
+using SouthernTreasuresBLL.Users;
+using SouthernTreasuresBLL.Users.Model;
+
+namespace SouthernTreasures.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LoginPage()
+        {
+            return View();
+        }
+
+        public ActionResult RegisterPage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ProcessLogin(UsersModel FormData)
+        {
+            UsersBLLModel UserTmp = new UsersBLLModel();
+            UserTmp = UserInfoBLL.getInstance().Retrieve(JsonConvert.DeserializeObject<UsersBLLModel>(JsonConvert.SerializeObject(FormData)));
+
+            return View();
+        }
+    }
+}
---
+++ SouthernTreasures/SouthernTreasures/Models/Offers.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace SouthernTreasures.Offers.Model
+{
+    public class OffersModel
+    {
+        private int _ID;
+        private int _UserID_Nbr;
+        private int _ProductID_Nbr;
+        private decimal _Price_Dec;
+        private DateTime _Submitted_DtTM;
+
+        public int ID
+        {
+            get { return _ID; }
+            set { _ID = value; }
+        }
+
+        public int UserID_Nbr
+        {
+            get { return _UserID_Nbr; }
+            set { _UserID_Nbr = value; }
+        }
+
+        public int ProductID_Nbr
+        {
+            get { return _ProductID_Nbr; }
+            set { _ProductID_Nbr = value; }
+        }
+
+        public decimal Price_Dec
+        {
+            get { return _Price_Dec; }
+            set { _Price_Dec = value; }
+        }
+
+        public DateTime Submitted_DtTM
+        {
+            get { return _Submitted_DtTM; }
+            set { _Submitted_DtTM = value; }
+        }
+
+        public OffersModel(int ID, int UserID_Nbr, int ProductID_Nbr, decimal Price_Dec, DateTime Submitted_DtTm)
+        {
+            this.ID = ID;
+            this.UserID_Nbr = UserID_Nbr;
+            this.ProductID_Nbr = ProductID_Nbr;
+            this.Price_Dec = Price_Dec;
+            this.Submitted_DtTM = Submitted_DtTM;
+        }
+
+        public OffersModel()
+        {
+
+        }
+    }
+}
