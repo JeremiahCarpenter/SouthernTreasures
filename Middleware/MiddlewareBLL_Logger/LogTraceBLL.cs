f93e8743779258984c2160c17a958469ace33c2e
SeanLeeDev <33349300+SeanLeeDev@users.noreply.github.com>
2019-11-06 15:52:54 -0600

    Add files via upload

---
+++ Middleware/MiddlewareBLL_Logger/LogTraceBLL.cs
@@ -0,0 +1,41 @@
+﻿using MiddlewareDAL.LogTrace.Models;
+using MiddlewareDAL.LogTrace;
+using MiddlewareBLL.LogTrace.Models;
+using Newtonsoft.Json;
+
+namespace MiddlewareBLL.LogTrace
+{
+    public class LogTraceBLL
+    {
+        static LogTraceBLL LogTrace_Ref = null;
+
+        public static LogTraceBLL getInstance()
+        {
+            if (LogTrace_Ref == null)
+            {
+                LogTrace_Ref = new LogTraceBLL();
+                return LogTrace_Ref;
+            }
+
+            else
+            {
+                return LogTrace_Ref;
+            }
+
+        }
+
+        public string Validate(LogTraceBLLModel Trace)
+        {
+            LogTraceDAL LTDAL = new LogTraceDAL();
+            string ReturnVal = LTDAL.Validate(JsonConvert.DeserializeObject<LogTraceDALModel>(JsonConvert.SerializeObject(Trace)));
+            return ReturnVal;
+        }
+
+        public string Create(LogTraceBLLModel Trace)
+        {
+            LogTraceDAL LTDAL = new LogTraceDAL();
+            string ReturnVal = LTDAL.Create(JsonConvert.DeserializeObject<LogTraceDALModel>(JsonConvert.SerializeObject(Trace)));
+            return ReturnVal;
+        }
+    }
+}
