4967f855f9216ad35b06d5ec436b397187ee849b
SeanLeeDev <33349300+SeanLeeDev@users.noreply.github.com>
2019-11-06 16:34:26 -0600

    Add files via upload

---
+++ SouthernTreasures/SouthernTreasuresDAL_Categories/CategoryInfoDAL.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using SouthernTreasuresDAL.Connection;
+using SouthernTreasuresDAL.Categories.Model;
+
+namespace SouthernTreasuresDAL.Categories
+{
+    public class CategoryInfoDAL
+    {
+        public string RetrieveSQL = "SELECT [ID], [Name_Txt] FROM Categories";
+        public DBConnectionStr SQLDB = new DBConnectionStr();
+
+        public List<CategoriesDALModel> Retrieve()
+        {
+            List<CategoriesDALModel> CategoryList = new List<CategoriesDALModel>();
+            using (SQLDB.SQLConnection)
+            {
+                try
+                {
+                    SQLDB.SQLConnection.Open();
+
+                    //Retrieve
+                    using (SqlCommand Cmd = new SqlCommand(RetrieveSQL, SQLDB.SQLConnection))
+                    {
+                        //Retrieve data
+                        using (SqlDataReader InpReader = Cmd.ExecuteReader())
+                        {
+                            while (InpReader.Read())
+                            {
+                                CategoryList.Add(new CategoriesDALModel(Convert.ToInt32(InpReader["ID"]), InpReader["Name_Txt"].ToString()));
+                            }
+                        }
+
+                        //Validate if valid
+                        if (CategoryList.Count == 0)
+                        {
+                            //REPLACE WITH MIDDLEWARE LOG WRITE.
+                            throw new Exception ("Nothing returned from the Categories table.");
+                        }
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    //REPLACE WITH MIDDLEWARE LOG WRITE.
+                    throw new Exception("Unable to access the SouthernTreasures database: " + ex);
+                }
+            }
+
+            return CategoryList;
+        }
+    }
+}
---
+++ SouthernTreasures/SouthernTreasuresDAL_Categories/Model/Categories.cs
@@ -0,0 +1,27 @@
+﻿namespace SouthernTreasuresDAL.Categories.Model
+{
+    public class CategoriesDALModel
+    {
+        private int _ID;
+        private string _Name_Txt;
+
+        public int ID
+        {
+            get { return _ID; }
+            set { _ID = value; }
+        }
+
+        public string Name_Txt
+        {
+            get { return _Name_Txt; }
+            set { _Name_Txt = value; }
+        }
+
+        public CategoriesDALModel(int ID, string Name_Txt)
+        {
+            this.ID = ID;
+            this.Name_Txt = Name_Txt;
+        }
+
+    }
+}
