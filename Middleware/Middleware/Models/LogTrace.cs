1066aa8cfef1484af5dea8fce3633990a7357245
SeanLeeDev <33349300+SeanLeeDev@users.noreply.github.com>
2019-11-06 16:06:44 -0600

    Add files via upload

---
+++ Middleware/Middleware/Models/LogTrace.cs
@@ -0,0 +1,27 @@
+﻿namespace Middleware.Models
+{
+    public class LogTrace
+    {
+        private string _AppName_Txt;
+        private string _Message_Txt;
+        private string _UserName_Txt;
+
+        public string AppName_Txt
+        {
+            get { return _AppName_Txt; }
+            set { _AppName_Txt = value; }
+        }
+
+        public string Message_Txt
+        {
+            get { return _Message_Txt; }
+            set { _Message_Txt = value; }
+        }
+
+        public string UserName_Txt
+        {
+            get { return _UserName_Txt; }
+            set { _UserName_Txt = value; }
+        }
+    }
+}
---
+++ Middleware/Middleware/Models/Users.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Middleware.Models
+{
+    [DataContract]
+    public class Users
+    {
+        private int _ID; 
+        private string _Name_Txt;
+        private string _Password_Txt;
+        private string _Email_Txt;
+        private decimal _Money_Dec;
+
+        [DataMember(Name = "ID")]
+        public int ID
+        {
+            get { return _ID; }
+            set { _ID = value; }
+        }
+
+        [DataMember(Name = "Name_Txt")]
+        public string Name_Txt
+        {
+            get { return _Name_Txt; }
+            set { _Name_Txt = value; }
+        }
+
+        [DataMember(Name = "Password_Txt")]
+        public string Password_Txt
+        {
+            get { return _Password_Txt; }
+            set { _Password_Txt = value; }
+        }
+
+        [DataMember(Name = "Email_Txt")]
+        public string Email_Txt
+        {
+            get { return _Email_Txt; }
+            set { _Email_Txt = value; }
+        }
+
+        [DataMember(Name = "Money_Dec")]
+        public Decimal Money_Dec
+        {
+            get { return _Money_Dec; }
+            set { _Money_Dec = value; }
+        }
+    }
+}
