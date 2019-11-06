223145370f497b6e5260a8bac86f18d271703ecd
SeanLeeDev <33349300+SeanLeeDev@users.noreply.github.com>
2019-11-06 15:44:03 -0600

    Add files via upload

---
+++ LoggingFramework/LoggingLogic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using LoggingFramework.Model;
+using System.Net.Http;
+using System.Configuration;
+using System.IO;
+
+namespace LoggingFramework
+{
+    public class LoggingLogic
+    {
+        private UriBuilder LoggingPath = new UriBuilder(ConfigurationManager.AppSettings["MiddlewareLogging"]);
+
+        public string CreateNewLogEntry(Logging LogInfo)
+        {
+            //Return string
+            String RetValue = "";
+
+            //Build arguments
+            LoggingPath.Query = "AppName_Txt=" + LogInfo.AppName_Txt + "&" + "Message_Txt=" + LogInfo.Message_Txt + "&" + "UserName_Txt=" + LogInfo.UserName_Txt;
+
+            //Call Service and Return Result
+            HttpClient client = new HttpClient();
+            var result = client.GetAsync(LoggingPath.Uri).Result;
+            using (StreamReader sr = new StreamReader(result.Content.ReadAsStreamAsync().Result))
+            {
+                RetValue = sr.ReadToEnd();
+            }
+
+            //Cleanup
+            client.Dispose();
+
+            return RetValue;
+        }
+    }
+}
---
+++ LoggingFramework/Model/Logging.cs
@@ -0,0 +1,34 @@
+﻿namespace LoggingFramework.Model
+{
+    public class Logging
+    {
+        private string _AppName_Txt;
+        private string _Message_Txt;
+        private string _UserName_Txt;
+
+        public string AppName_Txt
+        {
+            get { return _AppName_Txt; }
+            set { _AppName_Txt = value; }
+        }
+
+        public string Message_Txt
+        {
+            get { return _Message_Txt; }
+            set { _Message_Txt = value; }
+        }
+
+        public string UserName_Txt
+        {
+            get { return _UserName_Txt; }
+            set { _UserName_Txt = value; }
+        }
+
+        public Logging(string AppName_Txt, string Message_Txt, string UserName_Txt)
+        {
+            this.AppName_Txt = AppName_Txt;
+            this.Message_Txt = Message_Txt;
+            this.UserName_Txt = UserName_Txt;
+        }
+    }
+}
