bc8418e06c4372123df36cd7f7a9203aa9d2c93d
SeanLeeDev <33349300+SeanLeeDev@users.noreply.github.com>
2019-11-06 16:33:57 -0600

    Add files via upload

---
+++ SouthernTreasures/SouthernTreasuresDAL_Connection/DBConnectionStr.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+using System;
+
+namespace SouthernTreasuresDAL.Connection
+{
+    public class DBConnectionStr
+    {
+        public SqlConnection SQLConnection = new SqlConnection(new SqlConnectionStringBuilder()
+        {
+            DataSource = ConfigurationManager.AppSettings["DataSource"],
+            InitialCatalog = ConfigurationManager.AppSettings["InitialCatalog"],
+            IntegratedSecurity = Convert.ToBoolean(ConfigurationManager.AppSettings["IntegratedSecurity"]),
+            UserID = ConfigurationManager.AppSettings["UserID"],
+            Password = ConfigurationManager.AppSettings["Password"]
+        }.ConnectionString
+      );
+    }
+}
