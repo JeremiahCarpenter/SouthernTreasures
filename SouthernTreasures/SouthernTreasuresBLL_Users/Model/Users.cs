03fa8306fdbd4717787efa924a2af02442eec689
SeanLeeDev <33349300+SeanLeeDev@users.noreply.github.com>
2019-11-06 16:35:09 -0600

    Add files via upload

---
+++ SouthernTreasures/SouthernTreasuresBLL_Users/Model/Users.cs
@@ -0,0 +1,55 @@
+﻿namespace SouthernTreasuresBLL.Users.Model
+{
+    public class UsersBLLModel
+    {
+        private int _ID;
+        private string _Name_Txt;
+        private string _Password_Txt;
+        private string _Email_Txt;
+        private decimal _Money_Dec;
+
+        public int ID
+        {
+            get { return _ID; }
+            set { _ID = value; }
+        }
+
+        public string Name_Txt
+        {
+            get { return _Name_Txt; }
+            set { _Name_Txt = value; }
+        }
+
+        public string Password_Txt
+        {
+            get { return _Password_Txt; }
+            set { _Password_Txt = value; }
+        }
+
+        public string Email_Txt
+        {
+            get { return _Email_Txt; }
+            set { _Email_Txt = value; }
+        }
+
+        public decimal Money_Dec
+        {
+            get { return _Money_Dec; }
+            set { _Money_Dec = value; }
+        }
+
+        public UsersBLLModel(int ID, string Name_Txt, string Password_Txt, string Email_Txt, decimal Money_Dec)
+        {
+            this.ID = ID;
+            this.Name_Txt = Name_Txt;
+            this.Password_Txt = Password_Txt;
+            this.Email_Txt = Email_Txt;
+            this.Money_Dec = Money_Dec;
+        }
+
+        public UsersBLLModel()
+        {
+
+        }
+    }
+}
---
+++ SouthernTreasures/SouthernTreasuresBLL_Users/UserInfoBLL.cs
@@ -0,0 +1,69 @@
+﻿using SouthernTreasuresDAL.Users;
+using SouthernTreasuresDAL.Users.Model;
+using SouthernTreasuresBLL.Users.Model;
+using Newtonsoft.Json;
+
+namespace SouthernTreasuresBLL.Users
+{
+    public class UserInfoBLL
+    {
+        static UserInfoBLL Users_Ref = null;
+
+        public static UserInfoBLL getInstance()
+        {
+            if (Users_Ref == null)
+            {
+                Users_Ref = new UserInfoBLL();
+                return Users_Ref;
+            }
+
+            else
+            {
+                return Users_Ref;
+            }
+
+        }
+
+        public string Validate(UsersBLLModel UserInfo)
+        {
+            UserInfoDAL UDAL = new UserInfoDAL();
+            string ReturnVal = UDAL.Validate(JsonConvert.DeserializeObject<UsersDALModel>(JsonConvert.SerializeObject(UserInfo)));
+            return ReturnVal;
+        }
+
+        public string ValidateKey(UsersBLLModel UserInfo)
+        {
+            UserInfoDAL UDAL = new UserInfoDAL();
+            string ReturnVal = UDAL.ValidateKey(JsonConvert.DeserializeObject<UsersDALModel>(JsonConvert.SerializeObject(UserInfo)));
+            return ReturnVal;
+        }
+
+        public string Create(UsersBLLModel UserInfo)
+        {
+            UserInfoDAL UDAL = new UserInfoDAL();
+            string ReturnVal = UDAL.Create(JsonConvert.DeserializeObject<UsersDALModel>(JsonConvert.SerializeObject(UserInfo)));
+            return ReturnVal;
+        }
+
+        public UsersBLLModel Retrieve(UsersBLLModel UserInfo)
+        {
+            UserInfoDAL UDAL = new UserInfoDAL();
+            UsersBLLModel RetUserInfo = JsonConvert.DeserializeObject<UsersBLLModel>(JsonConvert.SerializeObject(UDAL.RetrieveByNameAndPass(JsonConvert.DeserializeObject<UsersDALModel>(JsonConvert.SerializeObject(UserInfo)))));               
+            return RetUserInfo;
+        }
+
+        public string Update(UsersBLLModel UserInfo)
+        {
+            UserInfoDAL UDAL = new UserInfoDAL();
+            string ReturnVal = UDAL.Update(JsonConvert.DeserializeObject<UsersDALModel>(JsonConvert.SerializeObject(UserInfo)));
+            return ReturnVal;
+        }
+
+        public string Delete(int ID)
+        {
+            UserInfoDAL UDAL = new UserInfoDAL();
+            string ReturnVal = UDAL.Delete(ID);
+            return ReturnVal;
+        }
+    }
+}
