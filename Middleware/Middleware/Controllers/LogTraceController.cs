8c2213c169026f30af8d5532e92573a1173fd3f2
SeanLeeDev <33349300+SeanLeeDev@users.noreply.github.com>
2019-11-06 16:05:57 -0600

    Add files via upload

---
+++ Middleware/Middleware/Controllers/LogTraceController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+using Middleware.Models;
+using MiddlewareBLL.LogTrace;
+using MiddlewareBLL.LogTrace.Models;
+using Newtonsoft.Json;
+
+namespace Middleware.Controllers
+{
+    public class LogTraceController : ApiController
+    {
+        [Route("api/AddLogTrace")]
+        [HttpPost]
+        public string AddLogTrace(LogTrace Trace)
+        {
+            //Run data validation
+            string ReturnErr = LogTraceBLL.getInstance().Validate(JsonConvert.DeserializeObject<LogTraceBLLModel>(JsonConvert.SerializeObject(Trace)));
+            if (ReturnErr != "") { return ReturnErr; }
+
+            //Execute Insert Request
+            string ReturnVal = LogTraceBLL.getInstance().Create(JsonConvert.DeserializeObject<LogTraceBLLModel>(JsonConvert.SerializeObject(Trace)));
+            return ReturnVal;
+        }
+    }
+}
---
+++ Middleware/Middleware/Controllers/UsersController.cs
@@ -0,0 +1,59 @@
+﻿using System.Web.Http;
+using Middleware.Models;
+using MiddlewareBLL.Users;
+using MiddlewareBLL.Users.Models;
+using Newtonsoft.Json;
+
+namespace Middleware.Controllers
+{
+    public class UsersController : ApiController
+    {
+        [Route("api/AddUser")]
+        [HttpPost]
+        public string AddUser(Users UserInfo)
+        {
+            //Run data validation
+            string ReturnErr = UsersBLL.getInstance().Validate(JsonConvert.DeserializeObject<UsersBLLModel>(JsonConvert.SerializeObject(UserInfo)));
+            if(ReturnErr != "") { return ReturnErr; }
+
+            //Execute Insert Request
+            string ReturnVal = UsersBLL.getInstance().Create(JsonConvert.DeserializeObject<UsersBLLModel>(JsonConvert.SerializeObject(UserInfo)));
+            return ReturnVal;
+        }
+
+        [Route("api/RetrieveUser")]
+        [HttpPost]
+        public string RetrieveUser(Users UserInfo)
+        {
+            //User Retrieve Request
+            string ReturnVal = UsersBLL.getInstance().Retrieve(JsonConvert.DeserializeObject<UsersBLLModel>(JsonConvert.SerializeObject(UserInfo)));
+            return ReturnVal;
+        }
+
+        [Route("api/UpdateUser")]
+        [HttpPost]
+        public string UpdateUser(Users UserInfo)
+        {
+            //Run data validation
+            string ReturnErr = UsersBLL.getInstance().ValidateKey(JsonConvert.DeserializeObject<UsersBLLModel>(JsonConvert.SerializeObject(UserInfo)));
+            if (ReturnErr != "") { return ReturnErr; }
+
+            //Execute Update Request
+            string ReturnVal = UsersBLL.getInstance().Update(JsonConvert.DeserializeObject<UsersBLLModel>(JsonConvert.SerializeObject(UserInfo)));
+            return ReturnVal;
+        }
+
+        [Route("api/DeleteUser")]
+        [HttpPost]
+        public string DeleteUser(Users UserInfo)
+        {
+            //Run data validation
+            string ReturnErr = UsersBLL.getInstance().ValidateKey(JsonConvert.DeserializeObject<UsersBLLModel>(JsonConvert.SerializeObject(UserInfo)));
+            if (ReturnErr != "") { return ReturnErr; }
+
+            //Execute Delete Request
+            string ReturnVal = UsersBLL.getInstance().Delete(JsonConvert.DeserializeObject<UsersBLLModel>(JsonConvert.SerializeObject(UserInfo)));
+            return ReturnVal;
+        }
+    }
+}
